d96fdc87b48b2801f84fbf6a331894248557d5cf
BenBasic <benjaminmbasic@gmail.com>
2022-06-30 10:35:29 -0400

    Created user-seeds, hopefully it works and doesn't run into issues with bulk create

---
+++ seeds/user-seeds.js
@@ -0,0 +1,38 @@
+// Setting up model requirment for User
+const { User } = require('../models');
+
+// Defining the properties of userData
+const userData = [
+    {
+        username: "Ben",
+        email: "ben@email.com",
+        password: "test12345"
+    },
+    {
+        username: "James",
+        email: "james@email.com",
+        password: "test12345"
+    },
+    {
+        username: "Andrew",
+        email: "andrew@email.com",
+        password: "test12345"
+    },
+    {
+        username: "Keegan",
+        email: "keegan@email.com",
+        password: "test12345"
+    },
+    {
+        username: "Bob",
+        email: "bob@email.com",
+        password: "test12345"
+    },
+];
+
+// seedUsers arrow function to bulk create User tables based on the userData defined above
+// NOTE TO SELF: In documentation I have seen conflicting things about bulkCreate and not working with hashed/unhashed passwords, will need to look into this with further testing
+const seedUsers = () => User.bulkCreate(userData);
+
+// Exports the module to be used in other files
+module.exports = seedUsers;
