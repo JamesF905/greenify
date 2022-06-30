8ffa6bf7f2727daad8df3f9686f280f4666ae1ad
BenBasic <benjaminmbasic@gmail.com>
2022-06-30 14:33:13 -0400

    Created item-routes, hoping to see if all of this works once connection is established

---
+++ controllers/api/item-routes.js
@@ -0,0 +1,160 @@
+// Setting up file requirements
+const router = require('express').Router();
+const { User, Item, History } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+// Gets all Posts table data
+router.get('/', (req, res) => {
+    Item.findAll({
+        // From the Item table, it will find all attributes including the item ID, text, title, and timestamp
+        attributes: 
+        [
+            'id',
+            'item_text',
+            'title',
+            'created_at',
+        ],
+        // Order the posts from most recent in descending order
+        order: [[ 'created_at', 'DESC']],
+        // From the User table, include the username from the User associated with the item. Then, from the History table, include all history edits (which requires their id, history_name, item_id, user_id, and timestamp properties)
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: History,
+                attributes: ['id', 'history_name', 'item_id', 'user_id', 'created_at'],
+                include: 
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData)) // Returning the result data as JSON Object
+    // if there is an error, it will log an error
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Get a single item by it's id
+router.get('/:id', (req, res) => {
+    Item.findOne({
+      where: 
+      {
+        // Checks for the request's id property, this will make it check for if the id parameter of both the request and the result match
+        id: req.params.id
+      },
+      // From the Item table, it will find all attributes including the item ID, text, title, and timestamp
+      attributes: 
+      [
+        'id',
+        'item_text',
+        'title',
+        'created_at',
+      ],
+      // From the User table, include the username from the User associated with the item. Then, from the History table, include all history edits (which requires their id, history_name, item_id, user_id, and timestamp properties)
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+            model: History,
+            attributes: ['id', 'history_name', 'item_id', 'user_id', 'created_at'],
+            include: 
+            {
+                model: User,
+                attributes: ['username']
+            }
+        }
+      ]
+    })
+    .then(dbPostData => {
+        // If there is no matching id for the post requested, log an error
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No item with this id exists' });
+          return;
+        }
+        res.json(dbPostData); // Returning the result data as JSON Object
+    })
+      .catch(err => {
+        // if there is an error, it will log an error
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Creates a new item
+router.post('/', withAuth, (req, res) => {
+    // Creates a new item in the Item table with the properties of title, item_text, and user_id
+    Item.create({
+        title: req.body.title,
+        post_text: req.body.item_text,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData)) // Returning the result data as JSON Object
+    .catch(err => {
+        // if there is an error, it will log an error
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Update a item's title or text for post that matches the requested id
+router.put('/:id', withAuth, (req, res) => {
+    Item.update(req.body,
+        {
+            // Checks for the request's id property, this will make it check for if the id parameter of both the request and the result match
+            where: 
+            {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        // If there is no matching id for the post requested, log an error
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No item with this id exists' });
+            return;
+        }
+        res.json(dbPostData); // Returning the result data as JSON Object
+    })
+    .catch(err => {
+        // if there is an error, it will log an error
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+// Deletes an item for an item that matches the requested id
+router.delete('/:id', withAuth, (req, res) => {
+    Item.destroy({
+      // Checks for the request's id property, this will make it check for if the id parameter of both the request and the result match
+      where: 
+      {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        // If there is no matching id for the post requested, log an error
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No item with this id exists' });
+          return;
+        }
+        res.json(dbPostData); // Returning the result data as JSON Object
+      })
+      .catch(err => {
+        // if there is an error, it will log an error
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Exports the module to be used in other files
+module.exports = router;
