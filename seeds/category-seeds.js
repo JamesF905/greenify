015c51f62166124c308fcfb6d1e81dce4f5a5bca
BenBasic <benjaminmbasic@gmail.com>
2022-06-30 10:47:15 -0400

    Created category-seeds, noticed its not importing the Category model properly, looks like its from me forgetting to include it in the model branch index.js, will fix that now

---
+++ seeds/category-seeds.js
@@ -0,0 +1,21 @@
+// Setting up model requirment for Category
+const { Category } = require('../models');
+
+// Defining the properties of categoryData
+const categoryData = [
+    {
+        category_name: "Indoor Plants",
+    },
+    {
+        category_name: "Outdoor Plants",
+    },
+    {
+        category_name: "Products",
+    },
+]
+
+// seedCategories arrow function to bulk create Category tables based on the categoryData defined above
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+// Exports the module to be used in other files
+module.exports = seedCategories;
